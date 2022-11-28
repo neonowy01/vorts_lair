da44b84b076a82115037b9467a6dc36fab8732a7
Neon <84806500+neonowy01@users.noreply.github.com>
2022-11-28 22:36:50 +0100

    the beginning

---
+++ scripts/vscripts/arena.lua
@@ -0,0 +1,27 @@
+switch = {
+	["grunt"] = function() return Entities:FindByName(nil, "grunt_template") end,
+	["captain"] = function() return Entities:FindByName(nil, "captain_template") end,
+	["supp"] = function() return Entities:FindByName(nil, "supp_template") end,
+}
+
+function spawn(npc, position)
+	template = switch[npc]()
+	realPos = Entities:FindByName(nil, position):GetOrigin()
+	print(realPos)
+	template:ForceSpawn()
+	npcTable = template:GetSpawnedEntities()
+	--print(#npcTable)
+	for i = #npcTable, 1, -1
+	do 	
+		print(i)
+		if npcTable[i]:GetClassname()=="npc_combine_s"
+			then
+			print(npcTable[i]:GetClassname())
+			print("Found")
+			finalNpc = npcTable[i]
+			break
+			end
+	end
+	finalNpc:SetOrigin(realPos)
+end
+
