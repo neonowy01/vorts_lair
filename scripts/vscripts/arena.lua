5f0dd52e9297ee52c9c67f47b737b39d1d5b541a
Neon <84806500+neonowy01@users.noreply.github.com>
2022-11-29 21:57:33 +0100

    Spawning barrels with the script; Added npc waves to the boss battle

--- scripts/vscripts/arena.lua
+++ scripts/vscripts/arena.lua
@@ -2,6 +2,7 @@ switch = {
 	["grunt"] = function() return Entities:FindByName(nil, "grunt_template") end,
 	["captain"] = function() return Entities:FindByName(nil, "captain_template") end,
 	["supp"] = function() return Entities:FindByName(nil, "supp_template") end,
+	["barrel"] = function() return Entities:FindByName(nil, "barrel_template") end,
 }
 
 function spawn(npc, position)
@@ -14,7 +15,7 @@ function spawn(npc, position)
 	for i = #npcTable, 1, -1
 	do 	
 		print(i)
-		if npcTable[i]:GetClassname()=="npc_combine_s"
+		if npcTable[i]:GetClassname()=="npc_combine_s" or npcTable[i]:GetName()=="barrel"
 			then
 			print(npcTable[i]:GetClassname())
 			print("Found")
@@ -24,4 +25,5 @@ function spawn(npc, position)
 	end
 	finalNpc:SetOrigin(realPos)
 end
+
 
